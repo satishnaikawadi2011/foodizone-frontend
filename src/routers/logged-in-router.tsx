0abf24f118618954a210490ff9536d8d520af669
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-12-25 20:13:40 +0530

    Added NotFound page and some client routes.

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import AppLoader from '../components/shared/AppLoader';
-import { useMeQuery } from '../generated/graphql';
+import { useMeQuery, UserRole } from '../generated/graphql';
+import Restaurants from '../pages/client/restaurants';
+import NotFound from '../pages/not-found';
+
+export interface RouteType {
+	path: string;
+	element: JSX.Element;
+}
+
+const ClientRoutes: RouteType[] = [
+	{ path: '/', element: <Restaurants /> }
+];
 
 const LoggedInRouter = () => {
 	const { data, loading, error } = useMeQuery();
@@ -13,7 +26,17 @@ const LoggedInRouter = () => {
 		);
 	}
 
-	return <div>{data.me.role}</div>;
+	return (
+		<Router>
+			<Routes>
+				{data.me.role === UserRole.Client &&
+					ClientRoutes.map((route) => {
+						return <Route path={route.path} element={route.element} />;
+					})}
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default LoggedInRouter;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateAccount from '../pages/create-account';
 import Login from '../pages/login';
+import NotFound from '../pages/not-found';
 const LoggedOutRouter = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Login />} />
 				<Route path="/create-account" element={<CreateAccount />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Router>
 	);
